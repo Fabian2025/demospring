defce4bad8b97b2a665186a2e99742f35f451330
Fabian2025 <cabrejoariasfabian@gmail.com>
2024-12-10 16:01:07 -0500

    add post, put, delete and patch endpoints

--- src/main/java/com/example/demo/controller/TestEndPoint.java
+++ src/main/java/com/example/demo/controller/TestEndPoint.java
@@ -1,14 +1,33 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestEndPoint {
 
-    @GetMapping("/test")
-    public String testEndPoint(){
-        return "Hello World";
+    @GetMapping("/gettest")
+    public String getTestEndPoint(){
+        return "Hello Get World";
+    }
+
+    @PostMapping("/posttest")
+    public String PostTestEndPoint(){
+        return "Hello Post World";
+    }
+
+    @PutMapping("/puttest")
+    public String putTestEndPoint(){
+        return "Hello Put World";
+    }
+
+    @DeleteMapping("/deletetest")
+    public String deleteTestEndPoint(){
+        return "Hello Delete World";
+    }
+
+    @PatchMapping("/patchtest")
+    public String patchTestEndPoint(){
+        return "Hello Patch World";
     }
 }
